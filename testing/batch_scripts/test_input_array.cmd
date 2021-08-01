52f5a9800cb465c92c0c4afebda6469a4c29597d
MaximilianParis <88265460+MaximilianParis@users.noreply.github.com>
2021-08-01 09:52:24 +0200

    Update test_input_array.cmd

--- testing/batch_scripts/test_input_array.cmd
+++ testing/batch_scripts/test_input_array.cmd
@@ -1,7 +1,7 @@
 cd C:\Users\maxim\source\repos\Main\Debug
 for /l %%x in ( 1,1, 100) do (
 echo %%x
- generate_array <C:\Users\maxim\Documents\primzahlen1mio.txt  %%x 
+ generate_array %%x 
 Timeout 1
 )
 cd C:\Users\maxim\Documents\batch_scripts
