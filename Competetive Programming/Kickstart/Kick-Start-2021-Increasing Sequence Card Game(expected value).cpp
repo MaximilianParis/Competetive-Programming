4b4b1a96bd49455ca668af110a34854c1091e9d8
MaximilianParis <88265460+MaximilianParis@users.noreply.github.com>
2021-08-24 01:14:59 +0200

    Add files via upload

---
+++ Competetive Programming/Kickstart/Kick-Start-2021-Increasing Sequence Card Game(expected value).cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const int N = (int)1e6;
+ vector<long double>sum(N);
+
+ll n;
+
+
+
+
+void precompute() {
+
+    for (int i = 1; i < 1e6; i++)
+        sum[i] += 1.0 / i + sum[i - 1];
+}
+
+long double Mascheroni = 0.57721566490153286060651209008240243104215933593992;
+
+
+
+
+int main() {
+    precompute();
+
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        long double ans = 0;
+        cin >> n;
+        ll a = (12.0L * n * n);
+        if (n >= 1e6)
+            ans = log((long double)n) + Mascheroni + 1 / (2.0L * n) - 1 / (12.0L * n * n);
+
+        else
+            ans = sum[n];
+        cout << "Case #" << tc << ": " << setprecision(20) << ans << endl;
+    }
+
+
+    return 0;
+}
