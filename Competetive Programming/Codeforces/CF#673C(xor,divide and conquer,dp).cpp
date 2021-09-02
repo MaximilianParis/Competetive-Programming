97e24b51abc623a51b9ac3cf459f0c050983e54f
MaximilianParis <88265460+MaximilianParis@users.noreply.github.com>
2021-09-03 01:14:42 +0200

    Update CF#673C(xor,divide and conquer,dp).cpp

--- Competetive Programming/Codeforces/CF#673C(xor,divide and conquer,dp).cpp
+++ Competetive Programming/Codeforces/CF#673C(xor,divide and conquer,dp).cpp
@@ -7,40 +7,34 @@ using namespace std;
 typedef long long ll;
 int n, m, d;
 vector<int>nums;
-
-ll ans=0;
-int x=0;
-pair<ll, ll> InversionsAtBit(vector<int>& values, int bit, int value);
+vector<tuple<int, int, int, int>> CountZeroAndOnes;
+ll ans = 0;
+int x = 0;
+pair<ll, ll> InversionsAtBit(vector<int>& values, int bit, int value,int pos);
 void dp(vector<int>& values) {
     vector<vector<int>> BitValues1(n);
     vector<vector<int>> BitValues2(n);
-    vector<vector<int>>& BitValues= BitValues1;
+    CountZeroAndOnes = vector<tuple<int, int, int, int>>(n);
+    vector<vector<int>>& BitValues = BitValues1;
     vector<vector<int>>& BitValues3 = BitValues2;
-    
+
     int maxbit = 29;
     int count = 0;
     for (int bit = maxbit; bit >= 0; bit--) {
-       
+
         if (bit == maxbit) {
-            
-            
-            pair<ll, ll> bitValues=InversionsAtBit(values, bit, 0);
+
+
+            pair<ll, ll> bitValues = InversionsAtBit(values, bit, 0,0);
             if (bitValues.first <= bitValues.second) {
                 ans += bitValues.first;
                 int value = 1 << bit;
                 int size = count;
                 for (int j = 0; j <= size; j++) {
 
-                    int cntZero = 0;
-                    int cntOne = 0;
-                    for (auto it : values) {
-                        it ^= x;
-                        if ((it & value) == value)
-                            cntOne++, it ^= x;
-                        else
-                            cntZero++, it ^= x;
-
-                    }
+                    int cntZero = get<0>(CountZeroAndOnes[j]);
+                    int cntOne = get<1>(CountZeroAndOnes[j]);
+                    
                     if (cntZero > 0 && cntOne > 0) {
                         BitValues2[count++] = vector<int>(cntZero);
                         BitValues2[count++] = vector<int>(cntOne);
@@ -88,16 +82,9 @@ void dp(vector<int>& values) {
                 int size = count;
                 for (int j = 0; j <= size; j++) {
 
-                    int cntZero = 0;
-                    int cntOne = 0;
-                    for (auto it : values) {
-                        it ^= x;
-                        if ((it & value) == value)
-                            cntOne++, it ^= x;
-                        else
-                            cntZero++, it ^= x;
+                    int cntZero = get<2>(CountZeroAndOnes[j]);
+                    int cntOne = get<3>(CountZeroAndOnes[j]);
 
-                    }
                     if (cntZero > 0 && cntOne > 0) {
                         BitValues2[count++] = vector<int>(cntZero);
                         BitValues2[count++] = vector<int>(cntOne);
@@ -137,141 +124,125 @@ void dp(vector<int>& values) {
 
                 }
                 swap(BitValues, BitValues3);
-               
-               
+
+
             }
         }
         else {
             ll sumBitOne = 0;
             ll sumBitZero = 0;
-       
+
             int cnt = 0;
             for (int j = 0; j < count; j++) {
-               
-                pair<ll, ll> bitValues1 = InversionsAtBit(BitValues[j], bit, x);
-               
+
+                pair<ll, ll> bitValues1 = InversionsAtBit(BitValues[j], bit, x,j);
+
                 sumBitZero += bitValues1.first;
-                    sumBitOne += bitValues1.second;
+                sumBitOne += bitValues1.second;
             }
-                
-                if (sumBitZero <= sumBitOne) {
-                    ans += sumBitZero;
-                    if (bit > 0) {
-                        int value = 1 << bit;
-                        int size = count;
-                        for (int j = 0; j < size; j++) {
-                            
-                            int cntZero = 0;
-                            int cntOne = 0;
+
+            if (sumBitZero <= sumBitOne) {
+                ans += sumBitZero;
+                if (bit > 0) {
+                    int value = 1 << bit;
+                    int size = count;
+                    for (int j = 0; j < size; j++) {
+
+                        int cntZero = get<0>(CountZeroAndOnes[j]);
+                        int cntOne = get<1>(CountZeroAndOnes[j]);
+                        if (cntZero > 0 && cntOne > 0) {
+                            BitValues2[cnt++] = vector<int>(cntZero);
+                            BitValues2[cnt++] = vector<int>(cntOne);
+                            int i = 0, k = 0;
                             for (auto it : BitValues[j]) {
                                 it ^= x;
                                 if ((it & value) == value)
-                                    cntOne++,it ^= x;
+                                    BitValues2[cnt - 1][i++] = it ^= x;
                                 else
-                                    cntZero++, it ^= x;
+                                    BitValues2[cnt - 2][k++] = it ^= x;
 
                             }
-                            if (cntZero > 0&& cntOne > 0) {
-                                BitValues2[cnt++] = vector<int>(cntZero);
-                                BitValues2[cnt++] = vector<int>(cntOne);
-                                int i = 0, k = 0;
-                                for (auto it : BitValues[j]) {
-                                    it ^= x;
-                                    if ((it & value) == value)
-                                        BitValues2[cnt -1][i++] = it ^= x;
-                                    else
-                                        BitValues2[cnt - 2][k++] = it ^= x;
-
-                                }
-                            }
-                            else if (cntOne > 0) {
-                                BitValues2[cnt++] = vector<int>(cntOne);
-                                int i = 0;
-                                for (auto it : BitValues[j]) {
-                                   
-                                   BitValues2[cnt - 1][i++] = it;
-                                   
-
-                                }
-                            }
-                            else if (cntZero > 0) {
-                                BitValues2[cnt++] = vector<int>(cntZero);
-                                int i = 0;
-                                for (auto it : BitValues[j]) {
-
-                                    BitValues2[cnt - 1][i++] = it;
-
-
-                                }
-                            }
-
-                          
-                           
-
                         }
-                        swap(BitValues, BitValues3);
+                        else if (cntOne > 0) {
+                            BitValues2[cnt++] = vector<int>(cntOne);
+                            int i = 0;
+                            for (auto it : BitValues[j]) {
+
+                                BitValues2[cnt - 1][i++] = it;
+
+
+                            }
+                        }
+                        else if (cntZero > 0) {
+                            BitValues2[cnt++] = vector<int>(cntZero);
+                            int i = 0;
+                            for (auto it : BitValues[j]) {
+
+                                BitValues2[cnt - 1][i++] = it;
+
+
+                            }
+                        }
+
+
+
 
                     }
+                    swap(BitValues, BitValues3);
+
                 }
-                else {
-                    ans += sumBitOne;
-                    x += 1 << bit;
-                    if (bit > 0) {
-                        int value = 1 << bit;
-                        int size = count;
-                        for (int j = 0; j < size; j++) {
+            }
+            else {
+                ans += sumBitOne;
+                x += 1 << bit;
+                if (bit > 0) {
+                    int value = 1 << bit;
+                    int size = count;
+                    for (int j = 0; j < size; j++) {
 
-                            int cntZero = 0;
-                            int cntOne = 0;
+                        int cntZero = get<2>(CountZeroAndOnes[j]);
+                        int cntOne = get<3>(CountZeroAndOnes[j]);
+                        if (cntZero > 0 && cntOne > 0) {
+                            BitValues2[cnt++] = vector<int>(cntZero);
+                            BitValues2[cnt++] = vector<int>(cntOne);
+                            int i = 0, k = 0;
                             for (auto it : BitValues[j]) {
                                 it ^= x;
                                 if ((it & value) == value)
-                                    cntOne++, it ^= x;
+                                    BitValues2[cnt - 1][i++] = it ^= x;
                                 else
-                                    cntZero++, it ^= x;
+                                    BitValues2[cnt - 2][k++] = it ^= x;
 
                             }
-                            if (cntZero > 0 && cntOne > 0) {
-                                BitValues2[cnt++] = vector<int>(cntZero);
-                                BitValues2[cnt++] = vector<int>(cntOne);
-                                int i = 0, k = 0;
-                                for (auto it : BitValues[j]) {
-                                    it ^= x;
-                                    if ((it & value) == value)
-                                        BitValues2[cnt - 1][i++] = it ^= x;
-                                    else
-                                        BitValues2[cnt - 2][k++] = it ^= x;
-
-                                }
-                            }
-                            else if (cntOne > 0) {
-                                BitValues2[cnt++] = vector<int>(cntOne);
-                                int i = 0;
-                                for (auto it : BitValues[j]) {
-
-                                    BitValues2[cnt - 1][i++] = it;
-
-
-                                }
-                            }
-                            else if (cntZero > 0) {
-                                BitValues2[cnt++] = vector<int>(cntZero);
-                                int i = 0;
-                                for (auto it : BitValues[j]) {
-
-                                    BitValues2[cnt - 1][i++] = it;
-
-
-                                }
-                            }
-
-
-
-
                         }
-                        swap(BitValues, BitValues3);
+                        else if (cntOne > 0) {
+                            BitValues2[cnt++] = vector<int>(cntOne);
+                            int i = 0;
+                            for (auto it : BitValues[j]) {
+
+                                BitValues2[cnt - 1][i++] = it;
+
+
+                            }
+                        }
+                        else if (cntZero > 0) {
+                            BitValues2[cnt++] = vector<int>(cntZero);
+                            int i = 0;
+                            for (auto it : BitValues[j]) {
+
+                                BitValues2[cnt - 1][i++] = it;
+
+
+                            }
+                        }
+
+
+
 
                     }
+                    swap(BitValues, BitValues3);
+
+                }
             }
             count = cnt;
         }
@@ -281,8 +252,8 @@ void dp(vector<int>& values) {
 
 
 
-pair<ll,ll> InversionsAtBit(vector<int>& values,int bit,int value) {
- 
+pair<ll, ll> InversionsAtBit(vector<int>& values, int bit, int value, int pos) {
+
     ll cntZero = 0;
     ll cntOne = 0;
     int value1 = 1 << bit;
@@ -292,14 +263,15 @@ pair<ll,ll> InversionsAtBit(vector<int>& values,int bit,int value) {
         if ((it & value1) == value1)
             cntOne++;
         else
-            inv1+= cntOne;
+            inv1 += cntOne, cntZero++;
         it ^= value;
-       
+
     }
-   
-    
+    get<0>(CountZeroAndOnes[pos]) = cntZero;
+    get<1>(CountZeroAndOnes[pos]) = cntOne;
+
     value += (1 << bit);
-   
+
     cntZero = 0;
     cntOne = 0;
     ll inv2 = 0;
@@ -308,21 +280,23 @@ pair<ll,ll> InversionsAtBit(vector<int>& values,int bit,int value) {
         if ((it & value1) == value1)
             cntOne++;
         else
-            inv2 += cntOne;
+            inv2 += cntOne, cntZero++;;
         it ^= value;
     }
-   
+    get<2>(CountZeroAndOnes[pos]) = cntZero;
+    get<3>(CountZeroAndOnes[pos]) = cntOne;
+
     return { inv1,inv2 };
 }
 int main() {
 
     cin >> n;
-   
-    
-    
+
+    nums = vector<int>(n);
+
     for (int i = 0; i < n; i++) {
         cin >> nums[i];
-        
+
     }
     dp(nums);
 
