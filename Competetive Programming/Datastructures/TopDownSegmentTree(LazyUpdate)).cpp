030a45f08dc6c120eb06ca4fc1f7d15a11eaa245
MaximilianParis <88265460+MaximilianParis@users.noreply.github.com>
2021-09-22 22:07:07 +0200

    Update TopDownSegmentTree(LazyUpdate)).cpp

--- Competetive Programming/Datastructures/TopDownSegmentTree(LazyUpdate)).cpp
+++ Competetive Programming/Datastructures/TopDownSegmentTree(LazyUpdate)).cpp
@@ -1,48 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+template<typename T>
+
 template<typename T>
 struct TopDownSegmentTree {
     // limit for array size
-     int n;
-     int N;
-     //tree
-     vector<T> tree;
-     //Lazy Update
-     vector<T> lazyUpdate;
-     //merge function
-     T(*merge)(T, T);
-     // add function
-     T(*add)(T, T);
-     // default Value
-     T defaultValue;
-    
+    int n;
+    int N;
+    //tree
+    vector<T> tree;
+    //Lazy Update
+    vector<T> lazyUpdate;
+    //merge function
+    T(*merge)(T, T);
+    // add function
+    T(*add)(T, T, int, int);
+    // default Value
+    T defaultValue;
 
-     //construct segment tree with specific default value and merge function
-     TopDownSegmentTree(int n, T defaultValue, T(*merge)(T, T), T(*add)(T, T)) {
-         this->n = n;
-         int pow = ceil(log2(n));
-         this->N = (2<<(pow))-1;
-         this->merge = merge;
-         this->defaultValue = defaultValue;
-         this->add = add;
-         tree = vector<T>(N, defaultValue);
-         lazyUpdate= vector<T>(N, defaultValue);
-     }
 
-     //construct segment tree with specific array
-     TopDownSegmentTree(vector<T>& arr,T defaultValue, T(*merge)(T, T), T(*add)(T, T)):
-         TopDownSegmentTree(arr.size(), defaultValue, merge, add) {
-         build(arr,0,n-1,0);
-     }
+    //construct segment tree with specific default value and merge function
+    TopDownSegmentTree(int n, T defaultValue, T(*merge)(T, T), T(*add)(T, T,int,int)) {
+        this->n = n;
+        int pow = ceil(log2(n));
+        this->N = (2 << (pow)) - 1;
+        this->merge = merge;
+        this->defaultValue = defaultValue;
+        this->add = add;
+        tree = vector<T>(N, defaultValue);
+        lazyUpdate = vector<T>(N, defaultValue);
+    }
 
-     int getMiddle(int l, int r) {
+    //construct segment tree with specific array
+    TopDownSegmentTree(vector<T>& arr, T defaultValue, T(*merge)(T, T), T(*add)(T, T, int, int)) :
+        TopDownSegmentTree(arr.size(), defaultValue, merge, add) {
+        build(arr, 0, n - 1, 0);
+    }
 
-         return l + (r - l) / 2;
+    int getMiddle(int l, int r) {
 
-     }
+        return l + (r - l) / 2;
+
+    }
 
     // function to build the tree
-    void build(vector<T>& arr,int l, int r, int pos)
+    void build(vector<T>& arr, int l, int r, int pos)
     {
         //if we are at leaf
         if (l == r)
@@ -56,22 +60,22 @@ struct TopDownSegmentTree {
         else {
 
             int mid = getMiddle(l, r);
-            build(arr,l, mid, pos * 2 + 1);
-            build(arr,mid + 1, r, pos * 2 + 2);
-            tree[pos] = merge(tree[2*pos+1], tree[2 * pos + 2]);
+            build(arr, l, mid, pos * 2 + 1);
+            build(arr, mid + 1, r, pos * 2 + 2);
+            tree[pos] = merge(tree[2 * pos + 1], tree[2 * pos + 2]);
             lazyUpdate[pos] = defaultValue;
         }
-        
+
     }
     // function to perfom update
     void update_Range_util(int l, int r, int& l1, int& r1, T& diff, int pos) {
         //continue updating
         if (lazyUpdate[pos] != defaultValue) {
 
-            tree[pos] = add(tree[pos],lazyUpdate[pos]);
+            tree[pos] = add(tree[pos], lazyUpdate[pos],l,r);
             if (l != r) {
-                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1], lazyUpdate[pos]);
-                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], lazyUpdate[pos]);
+                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1], lazyUpdate[pos], l, l);
+                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], lazyUpdate[pos], l, l);
 
             }
             lazyUpdate[pos] = defaultValue;
@@ -80,17 +84,17 @@ struct TopDownSegmentTree {
         if (r < l1 || l > r1)return;
         //in range
         if (l1 <= l && r <= r1) {
-            //not at child then add und postpone update
+            //not at child then add and postpone update
             if (l != r) {
 
-                tree[pos] = add(tree[pos],diff);
-                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1],diff);
-                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], diff);
+                tree[pos] = add(tree[pos], diff , l, r);
+                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1], diff, l, l);
+                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], diff, l, l);
 
             }
             // At child then add
             else
-                tree[pos] = add(tree[pos], diff);
+                tree[pos] = add(tree[pos], diff, l, r);
 
             return;
         }
@@ -103,18 +107,18 @@ struct TopDownSegmentTree {
     }
 
     void update_Range(int l1, int r1, T difference) {
-      return update_Range_util(0, n - 1, l1, r1, difference, 0);
-       }
+        return update_Range_util(0, n-1, l1, r1, difference, 0);
+    }
 
     // function to get merge values on interval [l, r]
     T query(int begin, int end, int& l, int& r, int pos)
     {  //continue updating
         if (lazyUpdate[pos] != defaultValue) {
 
-            tree[pos] = add(tree[pos], lazyUpdate[pos]);
-            if (l != r) {
-                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1], lazyUpdate[pos]);
-                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], lazyUpdate[pos]);
+            tree[pos] = add(tree[pos], lazyUpdate[pos], begin,end);
+            if (begin != end) {
+                lazyUpdate[pos * 2 + 1] = add(lazyUpdate[pos * 2 + 1], lazyUpdate[pos],l,l);
+                lazyUpdate[pos * 2 + 2] = add(lazyUpdate[pos * 2 + 2], lazyUpdate[pos],l,l);
 
             }
             lazyUpdate[pos] = defaultValue;
@@ -132,24 +136,24 @@ struct TopDownSegmentTree {
     }
 
     T query(int l, int r) {
-       return query(0, n - 1, l, r, 0);
+        return query(0, n - 1, l, r, 0);
     }
 };
-int merge(int x1,int x2) {
-    return min(x1,x2);
-}
-int add(int x1, int x2) {
-    return x1+x2;
+
+ll merge(ll x1, ll x2) {
+    return min(x1 ,x2);
 }
 
-
+ll add(ll x1, ll x2,int l,int r) {
+    return x1 + (r-l+1)*x2;
+}
 
 
 // driver program to test the above function
 int main()
 {
-    vector<int> a = {234,3212,220,232,676,344,656,2534};
-    TopDownSegmentTree<int> tree(a,(int)1e9,merge,add);
+    vector<ll> a = {234,3212,220,232,676,344,656,2534};
+    TopDownSegmentTree<ll> tree(a,(int)1e9,merge,add);
    
 
    
