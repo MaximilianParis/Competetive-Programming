50adcfec85ef385de1f1c5e82b5bdd8563207669
MaximilianParis <88265460+MaximilianParis@users.noreply.github.com>
2021-09-22 18:32:51 +0200

    Update TopDownSegmentTree.cpp

--- Competetive Programming/Datastructures/TopDownSegmentTree.cpp
+++ Competetive Programming/Datastructures/TopDownSegmentTree.cpp
@@ -16,7 +16,8 @@ struct TopDownSegmentTree {
     //construct segment tree with specific default value and merge function
     TopDownSegmentTree(int n, T defaultValue, T(*merge)(T, T)) {
         this->n = n;
-        this->N = 2 * n - 1;
+        int pow = ceil(log2(n));
+        this->N = (2 << (pow)) - 1;
         this->merge = merge;
         this->defaultValue = defaultValue;
         tree = vector<T>(N, defaultValue);
